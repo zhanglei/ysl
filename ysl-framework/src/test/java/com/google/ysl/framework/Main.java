dc5369ea0241ba17c3f4983a66e682e407a4bc54
zhanglei <zhanglei_job_email@163.com>
2011-05-08 11:22:50 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-framework/src/test/java/com/google/ysl/framework/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2011 YSL.
+ * All rights reserved.
+ */
+package com.google.ysl.framework;
+
+/**
+ * Main
+ * 
+ * @author zhanglei
+ */
+public class Main {
+
+    /**
+     * Main
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
