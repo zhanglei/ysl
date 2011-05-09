2971f2916d580f5ae80e5b6ecc51bdfff8d69232
zhanglei <zhanglei_job_email@163.com>
2011-05-09 19:35:59 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-framework/src/main/java/com/google/ysl/framework/common/hibernate/IdEntity.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: IdEntity.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ * @version 1.0
+ */
+package com.google.ysl.framework.common.hibernate;
+
+/**
+ * <p>Title: IdEntity</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ *
+ */
+public class IdEntity {
+
+}
---
+++ ysl-framework/src/main/java/com/google/ysl/framework/common/struts2/BaseStruts2Action.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: BaseStruts2Action.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ * @version 1.0
+ */
+package com.google.ysl.framework.common.struts2;
+
+/**
+ * <p>Title: BaseStruts2Action</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ *
+ */
+public class BaseStruts2Action {
+
+}
