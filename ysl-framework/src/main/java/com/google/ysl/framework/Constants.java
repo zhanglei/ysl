45feedfb718bb7dc5e81201cab944c42c60c7ed4
zhanglei <zhanglei_job_email@163.com>
2011-05-09 19:37:12 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

--- ysl-framework/src/main/java/com/google/ysl/framework/Constants.java
+++ ysl-framework/src/main/java/com/google/ysl/framework/Constants.java
@@ -4,7 +4,7 @@
  * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
  * <p>Company: </p>
  * @author zhanglei<zhanglei_job_email@163.com>
- * @date 2011-5-8
+ * @date May 9, 2011
  * @version 1.0
  */
 package com.google.ysl.framework;
@@ -14,7 +14,7 @@ package com.google.ysl.framework;
  * <p>Description: </p>
  * <p>Company: </p> 
  * @author zhanglei<zhanglei_job_email@163.com>
- * @date 2011-5-8
+ * @date May 9, 2011
  *
  */
 public class Constants {
