19e1442eab3bfd6546977b8fe149b8aaf7295c0c
zhanglei <zhanglei_job_email@163.com>
2011-05-09 18:54:25 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

--- ysl-framework/src/test/java/com/google/ysl/framework/Main.java
+++ ysl-framework/src/test/java/com/google/ysl/framework/Main.java
@@ -1,23 +1,34 @@
-/*
- * Copyright (C) 2011 YSL.
- * All rights reserved.
+/**
+ * <p>Title: Main.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ * @version 1.0
  */
 package com.google.ysl.framework;
 
 /**
- * Main
- * 
- * @author zhanglei
+ * <p>Title: Main</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 9, 2011
+ *
  */
 public class Main {
 
-    /**
-     * Main
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
+	/**
+	 * <p>Title: main</p>
+	 * <p>Description: </p>
+	 * @param @param args
+	 * @return void
+	 * @throws
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-    }
+	}
 
 }
