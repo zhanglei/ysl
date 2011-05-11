7bb111a3e58aa11cf423e74349bcce311a9d4d3f
zhanglei <zhanglei_job_email@163.com>
2011-05-11 11:36:24 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-spider/src/main/java/com/google/ysl/spider/App.java
@@ -0,0 +1,34 @@
+/**
+ * <p>Title: App.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 11, 2011
+ * @version 1.0
+ */
+package com.google.ysl.spider;
+
+/**
+ * <p>Title: App</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 11, 2011
+ *
+ */
+public class App {
+
+	/**
+	 * <p>Title: main</p>
+	 * <p>Description: </p>
+	 * @param @param args
+	 * @return void
+	 * @throws
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
