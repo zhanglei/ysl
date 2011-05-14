0fb7c061d517c8328e255703453f9f5f0c6d28c1
zhanglei <zhanglei_job_email@163.com>
2011-05-14 11:51:18 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-cas/src/main/java/com/google/ysl/cas/Constants.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: Constants.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 14, 2011
+ * @version 1.0
+ */
+package com.google.ysl.cas;
+
+/**
+ * <p>Title: Constants</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 14, 2011
+ *
+ */
+public class Constants {
+
+}
---
+++ ysl-cas/src/test/java/com/google/ysl/cas/Main.java
@@ -0,0 +1,33 @@
+/**
+ * <p>Title: Main.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 14, 2011
+ * @version 1.0
+ */
+package com.google.ysl.cas;
+
+/**
+ * <p>Title: Main</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 14, 2011
+ *
+ */
+public class Main {
+
+	/**
+	 * <p>Title: main</p>
+	 * <p>Description: </p>
+	 * @param args
+	 * @return void
+	 * @throws
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
