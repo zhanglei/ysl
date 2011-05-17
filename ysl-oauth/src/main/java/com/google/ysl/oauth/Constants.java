13329d2756b0837ab7f07c04a294f551c6581e93
zhanglei <zhanglei_job_email@163.com>
2011-05-17 22:23:56 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

--- ysl-oauth/src/main/java/com/google/ysl/oauth/Constants.java
+++ ysl-oauth/src/main/java/com/google/ysl/oauth/Constants.java
@@ -19,4 +19,43 @@ package com.google.ysl.oauth;
  */
 public class Constants {
 
+	/**
+	 * @Fields server name :
+	 */
+	public static final String SERVER_NAME = "ysl-oauth";
+
+	/**
+	 * @Fields session key :
+	 */
+	public static final String SESSION_KEY = "_session_key_";
+
+	/**
+	 * @Fields session expiry time :
+	 */
+	public static final int SESSION_EXPIRY_TIME = 60 * 60;
+
+	/**
+	 * @Fields cookie path :
+	 */
+	public static final String COOKIE_PATH = "/";
+
+	/**
+	 * @Fields cookie domain :
+	 */
+	public static final String COOKIE_DOMAIN = ".ysl.com";
+
+	/**
+	 * @Fields cookie expiry time :
+	 */
+	public static final int COOKIE_EXPIRY_TIME = 60 * 60;
+
+	/**
+	 * @Fields default encoding :
+	 */
+	public static final String ENCODING_DEFAULT = "UTF-8";
+
+	/**
+	 * @Fields File separator from System properties :
+	 */
+	public static final String FILE_SEP = System.getProperty("file.separator");
 }
