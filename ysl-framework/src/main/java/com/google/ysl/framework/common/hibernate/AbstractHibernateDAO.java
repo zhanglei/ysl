1e5330d59d3671491f1cd371ef20c7eb0b1e5317
zhanglei <zhanglei_job_email@163.com>
2011-05-26 11:44:25 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-framework/src/main/java/com/google/ysl/framework/common/hibernate/AbstractHibernateDAO.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: AbstractHibernateDAO.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 26, 2011
+ * @version 1.0
+ */
+package com.google.ysl.framework.common.hibernate;
+
+/**
+ * <p>Title: AbstractHibernateDAO</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 26, 2011
+ *
+ */
+public class AbstractHibernateDAO {
+
+}
