f2645db10c0c53af0a0be776eb409c2d5277fa98
zhanglei <zhanglei_job_email@163.com>
2011-05-28 13:39:40 +0800

    Signed-off-by: zhanglei <zhanglei_job_email@163.com>

---
+++ ysl-framework/src/main/java/com/google/ysl/framework/common/spring/mvc/AbstractSpringController.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: AbstractSpringController.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 28, 2011
+ * @version 1.0
+ */
+package com.google.ysl.framework.common.spring.mvc;
+
+/**
+ * <p>Title: AbstractSpringController</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 28, 2011
+ *
+ */
+public class AbstractSpringController {
+
+}
---
+++ ysl-framework/src/main/java/com/google/ysl/framework/common/spring/mvc/interceptor/SharedRenderVariableInterceptor.java
@@ -0,0 +1,22 @@
+/**
+ * <p>Title: SharedRenderVariableInterceptor.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (C) 2011 YSL. All rights reserved. </p> 
+ * <p>Company: </p>
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 28, 2011
+ * @version 1.0
+ */
+package com.google.ysl.framework.common.spring.mvc.interceptor;
+
+/**
+ * <p>Title: SharedRenderVariableInterceptor</p>
+ * <p>Description: </p>
+ * <p>Company: </p> 
+ * @author zhanglei<zhanglei_job_email@163.com>
+ * @date May 28, 2011
+ *
+ */
+public class SharedRenderVariableInterceptor {
+
+}
